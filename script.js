f6ae047084a28d3f0a6e208c3732f5d05c6aad4b
Josh <joshbristow93@gmail.com>
2018-12-10 14:31:53 +0200

    re upload with correct solution

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var list = document.getElementsByTagName("li");
+//From github.com/ferkev/javascript/blob/master
 
-function inputLength() {
+var button = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul');
+var li = document.getElementsByTagName('li')
+
+button.addEventListener('click', Onclick);
+input.addEventListener('keypress', addElement);
+
+
+liEvent();
+buttonListElement();
+
+function checkInputlength(){
 	return input.value.length;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+
+function createListElement(){
+
+		var li = document.createElement("li");
+		var button = document.createElement('button');
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		li.appendChild(button)
+		button.innerHTML = "delete";
+		input.value = "";
+
+		liEvent();
+		buttonListElement();
 }
 
-function addListAfterClick() {
- 	if (inputLength() > 0) {
- 		createListElement();	
- 	}	
-}
 
-function addListAfterKeypress(event) {
- 	if (inputLength() > 0 && event.keyCode === 13) {
+
+function Onclick(){
+
+	if(checkInputlength() > 0){
 		createListElement();
- 	}	
+	}
+
 }
 
-button.addEventListener("click", addListAfterClick)
 
-input.addEventListener("keypress", addListAfterKeypress)
+function addElement(){
+
+	if(checkInputlength() > 0 && event.keyCode === 13){
+		createListElement();
+	}
+}
+
+function liEvent(){
+	for( i=0; i<li.length; i++){
+	li[i].addEventListener('click', changeClass)
+	}
+}
+
+function changeClass(){
+	this.classList.toggle('done');
+}
+
+function buttonListElement(){
+	var button = document.querySelectorAll('li button');
+	for( i=0; i<button.length; i++){
+	button[i].addEventListener('click', clearElement)
+	}
+}
+
+function clearElement(){
+	for(var i=0; i<li.length; i++){
+		this.parentNode.remove()
+	}
+}
